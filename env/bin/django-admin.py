96ebf2d2c422b0225162fe6aae08f7bde4d2af75
krunal <krunal@localhost.localdomain>
2024-04-16 23:11:42 +0530

    Before model change

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/krunal/Desktop/Intership/sel/ECommerceSite-Django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ static/js/helper.js
@@ -0,0 +1,5 @@
+
+const data = document.currentScript.dataset;
+console.log(data.products)
+var d = JSON.parse(data.products)
+console.log(d)
